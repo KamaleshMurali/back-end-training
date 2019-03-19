e22b0cf66a68ee2cd0419ee46c8a9370194355dd
OFS1\kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-19 11:08:12 +0530

    this configuration file is used to scan the available components

---
+++ online-shopping-application/src/com/ofs/training/resource/MyWebMVC.java
@@ -0,0 +1,13 @@
+package com.ofs.training.resource;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.ofs")
+public class MyWebMVC implements WebMvcConfigurer{
+
+}
