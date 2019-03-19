b3d4db68873500b450965587bdc5f96c41a4d2fe
OFS1\kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-19 17:19:14 +0530

    added @proxy annotation

--- online-shopping-application/src/com/ofs/training/model/User.java
+++ online-shopping-application/src/com/ofs/training/model/User.java
@@ -3,14 +3,14 @@
  */
 package com.ofs.training.model;
 
-import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Proxy;
 
 
 /**
@@ -19,6 +19,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name="USER")
+@Proxy(lazy=false)
 public class User {
 
     @Id
