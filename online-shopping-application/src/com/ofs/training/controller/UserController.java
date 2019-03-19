0e22b5c18ae03c5b52d61fb58806b6a86b6b1141
OFS1\kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-19 17:06:15 +0530

    user controller to perform user crud operation

--- online-shopping-application/src/com/ofs/training/controller/UserController.java
+++ online-shopping-application/src/com/ofs/training/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.ofs.training.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,6 +34,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
     @GetMapping
     private ResponseEntity<String> getUsers() {
 
@@ -49,15 +55,79 @@ public class UserController {
         }
         return new ResponseEntity<String>(result, HttpStatus.OK);
     }
+
     @GetMapping
     @RequestMapping("/{id}")
     private ResponseEntity<String> getUser(@PathVariable("id") Long id) {
 
         User user = userService.getUser(id);
+        System.out.println(user.toString());
         ObjectMapper obj = new ObjectMapper();
         String result = null;
         try {
             result = obj.writeValueAsString(user);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new ResponseEntity<String>(result, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @RequestMapping("/post")
+    private ResponseEntity<String> create(@RequestBody User user) {
+
+//        User userOne = new User();
+//        userOne.setId(20l);
+//        userOne.setFirstName("kamalesh");
+//        userOne.setLastName("murali");
+//        userOne.setEmail("kamalesh.murali@object-frontier.com");
+//        userOne.setAddress("chennai");
+
+        User createdUser = userService.create(user);
+        ObjectMapper obj = new ObjectMapper();
+        String result = null;
+        try {
+            result = obj.writeValueAsString(createdUser);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new ResponseEntity<String>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    @RequestMapping("/delete/{id}")
+    private ResponseEntity<String> delete(@PathVariable("id") long id) {
+
+        userService.delete(id);
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+    
+    @PutMapping
+    private ResponseEntity<String> update(@RequestBody User user) {
+
+        User updatedUser = userService.update(user);
+        ObjectMapper obj = new ObjectMapper();
+        String result = null;
+        try {
+            result = obj.writeValueAsString(updatedUser);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new ResponseEntity<String>(result, HttpStatus.OK);
+    }
+
+    @GetMapping
+    @RequestMapping("/readAll")
+    private ResponseEntity<String> readAll() {
+
+        List<User> users = userService.readAll();
+        ObjectMapper obj = new ObjectMapper();
+        String result = null;
+        try {
+            result = obj.writeValueAsString(users);
         } catch (JsonProcessingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
